ad73f10e90418f06a6e15512e8e1d61af5b9a12d
Fuzzy Lump <8ca53525@opayq.com>
2020-07-03 10:21:19 +1000

    added logic to python file. performed a cheese search and saved the har file.

--- test/testing_har.py
+++ test/testing_har.py
@@ -1,6 +1,9 @@
 import json
 from haralyzer import HarParser, HarPage
 import urllib.parse
+import os
+
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 with open('www.woolworths.com.au.har', 'r') as f:
     #har_parser = HarParser(json.loads(f.read()))
@@ -11,11 +14,26 @@ post_requests = har_page.post_requests
 # filter for adobe calls
 # contains https://woolworthsfoodgroup.sc.omtrdc
 
+adobe_hits = []
+for request in post_requests:
+    if "https://woolworthsfoodgroup.sc.omtrdc" in request["request"]["url"]:
+        adobe_hits.append(request)
+
 # text for call
-post_requests[4]["request"]["postData"]["text"]
+# post_requests[4]["request"]["postData"]["text"]
+'''
+print(json.dumps(post_requests[4], indent=4))
+print()
+print()
+'''
 
 # decode url characters
-data_sent = urllib.parse.unquote(post_requests[4]["request"]["postData"]["text"])
+# data_sent = urllib.parse.unquote(post_requests[4]["request"]["postData"]["text"])
+for adobe_hit in adobe_hits:
+    data_sent = urllib.parse.unquote(adobe_hit["request"]["postData"]["text"])
+    for i in data_sent.split("&"):
+        print(i)
 
-for i in data_sent.split("&"):
-    print(i)
+
+# one method to parse post requests
+# another method to parse get requests
